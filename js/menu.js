e2008006f5e67faec0768592ada54411106e052e
Victor Henrique <vhbatistafortunato2@gmail.com>
2023-01-09 18:36:22 -0300

    terminei o menu responsivo e tambem a parte da introdução

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+function menuShow(){
+  let menuMobile = document.querySelector('.menu-mobile');
+  if (menuMobile.classList.contains('open')){
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = "./img/icones/menu-hamburguer.svg";
+  } else {
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = "./img/icones/icone-fechar.svg";
+
+  }
+}
